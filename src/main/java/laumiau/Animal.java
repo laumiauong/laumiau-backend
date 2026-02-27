32ca019f6f6480d58654ef5aa0bca84e3e6da826
xurumelx <natalyoenningrosa05@gmail.com>
2026-02-27 16:17:53 -0300

    Novos atributos para a classe Animal

--- src/main/java/laumiau/Animal.java
+++ src/main/java/laumiau/Animal.java
@@ -10,14 +10,20 @@ public class Animal {
     private String raca;
     private int idade;
     private String sexo;
+    private boolean adotado;
+    private String vacinado;
+    private String porte;
 
-    public Animal(String nome, String especie, String raca, int idade, String sexo) {
+    public Animal(String nome, String especie, String raca, int idade, String sexo, boolean adotado, String vacinado, String porte) {
         this.id = contadorId++;
         this.nome = nome;
         this.especie = especie;
         this.raca = raca;
         this.idade = idade;
         this.sexo = sexo;
+        this.adotado = false;
+        this.vacinado = vacinado;
+        this.porte = porte;
     }
 
     public String getNome() {
@@ -62,6 +68,42 @@ public class Animal {
 
     public long getId() {
         return id;
+    }
+
+    public boolean isAdotado() {
+        return adotado;
+    }
+
+    public String getVacinado(){
+        return vacinado;
+    }
+
+    public void setVacinado(String vacinado){
+        this.vacinado = vacinado;
+    }
+
+    public String getPorte(){
+        return porte;
+    }
+
+    public void setPorte(String porte){
+        this.porte = porte;
+    }
+
+    public void adotar() {
+        this.adotado = true;
+    }
+
+    public void exibirDados(){
+        System.out.println("ID: "+ id);
+        System.out.println("Nome: "+ nome);
+        System.out.println("Espécie: "+ especie);
+        System.out.println("Raça: "+ raca);
+        System.out.println("Idade: "+ idade);
+        System.out.println("Sexo: "+ sexo);
+        System.out.println("Adotado: "+ (adotado ? "Sim" : "Não"));
+        System.out.println("Vacinado: "+ vacinado);
+        System.out.println("Porte: "+ porte);
     }
 
 }
