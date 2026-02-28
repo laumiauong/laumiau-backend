35b835004f9291e5a29015b435b00ecdd980dfa9
xurumelx <natalyoenningrosa05@gmail.com>
2026-02-28 10:13:30 -0300

    Protótipo

--- src/main/java/laumiau/Main.java
+++ src/main/java/laumiau/Main.java
@@ -3,20 +3,6 @@ package laumiau;
 
 public class Main {
     public static void main(String[] args) {
-        Animal b1 = new Animal(
-            "Luna",
-            "Cachorro",
-            "Daschund",
-            1,
-            Sexo.FEMEA,
-            true,
-            Porte.PEQUENO);
-
-System.out.println("--- LISTA DE ANIMAIS ----");
-        b1.exibirDados();
-        System.out.println("----------------------------------------");
-
-
 
     }
 }
