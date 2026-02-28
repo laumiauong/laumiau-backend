812accf752ef554a992d54aa4fb46507af5dc489
may <mayramartins133@gmail.com>
2026-02-28 14:40:24 -0300

    Base atualizada do usuário

--- src/main/java/laumiau/Usuario.java
+++ src/main/java/laumiau/Usuario.java
@@ -15,4 +15,50 @@ public class Usuario {
 
     public Usuario() {
     }
+
+    public Usuario(Long id, String nome, String email, String senha, TipoUsuario tipoUsuario) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean autenticar(String senhaDigitada) {
+        return this.senha.equals(senhaDigitada);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(TipoUsuario tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
 }
+
