93ca55dff6fc28ac3bcbd6a3a803adab7ae68f86
xurumelx <natalyoenningrosa05@gmail.com>
2026-05-13 08:36:28 -0300

    Alteração roundedPanel 1

--- src/main/java/laumiau/view/RoundedPanel.java
+++ src/main/java/laumiau/view/RoundedPanel.java
@@ -6,8 +6,8 @@ import java.awt.geom.RoundRectangle2D;
 
 public class RoundedPanel extends JPanel {
 
-    private int radius;
-    private Color backgroundColor;
+    private final int radius;
+    private final Color backgroundColor;
 
     public RoundedPanel(int radius, Color backgroundColor) {
         this.radius = radius;
@@ -17,24 +17,16 @@ public class RoundedPanel extends JPanel {
 
     @Override
     protected void paintComponent(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g.create();
+        super.paintComponent(g);
 
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         g2.setColor(backgroundColor);
-
         g2.fill(new RoundRectangle2D.Double(
-                0,
-                0,
-                getWidth(),
-                getHeight(),
-                radius,
-                radius
+                0, 0, getWidth(), getHeight(), radius, radius
         ));
 
         g2.dispose();
-
-        super.paintComponent(g);
     }
 }
